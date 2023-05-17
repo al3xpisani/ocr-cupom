aa94fc7064f23a934cd609918433e27439afd6b5
al3xpisani <alexandre.pisani.ant@gmail.com>
2023-05-17 13:16:12 -0300

    ocr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useMemo } from "react";
+import Tesseract from "tesseract.js";
 
 function App() {
+  const [ocr, setOcr] = useState("Recognizing...");
+  useEffect(() => {
+    doOCR();
+  }, []);
+
+  const doOCR = async () => {
+    let timeStarting = new Date().getTime();
+    console.log("starting ......");
+
+    //Nota jaú serve
+    // Tesseract.recognize("https://i.ibb.co/zh91bjb/IMG-4266.jpg", "por", {
+    //   logger: ({ status, progress }) => {
+    //     console.log(status, ` -> ` + Math.round(progress * 100) + ` %`);
+    //   },
+    // }).then(({ data: { text } }) => {
+
+    //cupom fiscal Bahia
+    // Tesseract.recognize("https://i.ibb.co/8YyB26g/image.png", "por", {
+    //   logger: ({ status, progress }) => {
+    //     console.log(status, ` -> ` + Math.round(progress * 100) + ` %`);
+    //   },
+    // }).then(({ data: { text } }) => {
+
+    //cupom fiscal bahia
+    // Tesseract.recognize("https://i.ibb.co/mcndD0Q/image.png", "por", {
+    //   logger: ({ status, progress }) => {
+    //     console.log(status, ` -> ` + Math.round(progress * 100) + ` %`);
+    //   },
+    // }).then(({ data: { text } }) => {
+
+    //cupom ceará
+    Tesseract.recognize("https://i.ibb.co/Lv0fJS6/image.png", "por", {
+      logger: ({ status, progress }) => {
+        console.log(status, ` -> ` + Math.round(progress * 100) + ` %`);
+      },
+    }).then(({ data: { text } }) => {
+      // console.log("text recognized.... ", text);
+
+      let timeEnding = new Date().getTime();
+      var difference = timeEnding - timeStarting;
+      var minutesDifference = Math.floor(difference / 1000 / 60);
+      difference -= minutesDifference * 1000 * 60;
+      var secondsDifference = Math.floor(difference / 1000);
+
+      console.log(
+        "End recognition in minutes ................",
+        minutesDifference
+      );
+      console.log(
+        "End recognition in seconds ................",
+        secondsDifference
+      );
+      setOcr(text);
+
+      const invoiceExtractedLines = [];
+      const invoiceContent = text.split("\n");
+      console.log("Reading text..............");
+      invoiceContent.forEach((element) => {
+        invoiceExtractedLines.push(element);
+      });
+      console.log(invoiceExtractedLines);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{ocr}</p>
     </div>
   );
 }
